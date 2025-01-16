9b31a95e952871119cbdf88d509b41fa588ad7c1
Arman Rajib <ahrajib6789@gmail.com>
2025-01-17 00:12:38 +0600

    Respond to url parameters and handle individual tour GET requests

--- app.js
+++ app.js
@@ -40,6 +40,24 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
