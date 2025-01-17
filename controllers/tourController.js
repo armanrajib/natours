3436991b2f7bd7e48bc02073e01479280c55c11b
Arman Rajib <ahrajib6789@gmail.com>
2025-01-18 01:27:19 +0600

    Implement pagination (page & limit) (making the api better)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -42,6 +42,19 @@ const getAllTours = async (req, res) => {
       console.log('fields:', 'Every fields except __v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    console.log('page:', page, 'limit:', limit, 'skip:', skip);
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
