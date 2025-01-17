f992ab0c1047272ac86bd97ed5cd934a311263e7
Arman Rajib <ahrajib6789@gmail.com>
2025-01-17 22:24:39 +0600

    Clean up the tourRoutes and tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,29 +1,4 @@
-import fs from 'fs';
-import appRoot from 'app-root-path';
-
 import Tour from '../models/tourModel.js';
-
-const tours = JSON.parse(
-  fs.readFileSync(`${appRoot}/dev-data/data/tours-simple.json`),
-);
-
-// CONTROLLER FOR PARAM MIDDLEWARE
-// --------------------------------
-
-// const checkTourId = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-
-//   const tourId = Number(val);
-//   const tour = tours.find((el) => el.id === tourId);
-
-//   if (!tour)
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-
-//   next();
-// };
 
 // CONTROLLERS
 // ============
@@ -124,26 +99,4 @@ const deleteTour = async (req, res) => {
   }
 };
 
-// OTHER CONTROLLERS
-// ------------------
-
-// const checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or price',
-//     });
-//   }
-
-//   next();
-// };
-
-export {
-  getAllTours,
-  createTour,
-  getTour,
-  updateTour,
-  deleteTour,
-  // checkTourId,
-  // checkBody,
-};
+export { getAllTours, createTour, getTour, updateTour, deleteTour };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,16 +6,11 @@ import {
   getTour,
   updateTour,
   deleteTour,
-  // checkTourId,
-  // checkBody,
 } from '../controllers/tourController.js';
 
 const router = express.Router();
 
-// PARAM MIDDLEWARE
-// router.param('id', checkTourId);
-
-router.route('/').get(getAllTours).post(createTour); // checkBody is removed from the POST route
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 export default router;
