15dd50b7751328519505e8f817de45255aec9ea6
Arman Rajib <ahrajib6789@gmail.com>
2025-01-20 17:55:41 +0600

    Implement data sanitization against NOSQL QUERY INJECTION and CROSS-SITE SCRIPTING (XSS)

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import appRoot from 'app-root-path';
 import dotenv from 'dotenv';
 import { rateLimit } from 'express-rate-limit';
 import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xssClean from 'xss-clean';
 
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
@@ -45,6 +47,12 @@ app.use('/api', limiter);
 
 // MIDDLEWARE 2: It parses incoming requests with a JSON payload & attaches the resulting JavaScript object to the req.body
 app.use(express.json({ limit: '10kb' })); // body more than 10kb will be rejected
+
+// ANOTHER MIDDLEWARE (DATA SANITIZATION AGAINST NOSQL QUERY INJECTION) - MONGODB
+app.use(mongoSanitize());
+
+// ANOTHER MIDDLEWARE (DATA SANITIZATION AGAINST XSS) - MONGODB
+app.use(xssClean());
 
 // MIDDLEWARE 3
 app.use((req, res, next) => {
