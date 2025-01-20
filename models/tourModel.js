2e67f853867197c3527200aa7d1214ab56b80556
Arman Rajib <ahrajib6789@gmail.com>
2025-01-21 03:27:59 +0600

    Implement modelling tour guides through (embedding)

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import slugify from 'slugify';
 import validator from 'validator';
 
+import User from './userModel.js';
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -104,6 +106,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -130,6 +133,13 @@ tourSchema.pre('save', function (next) {
 // CAN USE MULTIPLE PRE & POST MIDDLEWARES
 tourSchema.pre('save', function (next) {
   console.log('Will save document...');
+  next();
+});
+
+// EMBEDDING [guides] documents into the [tour] document
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
