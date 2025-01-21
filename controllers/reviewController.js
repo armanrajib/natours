808a29afcd9b23ee2f729516f62c4ebb5e07cee1
Arman Rajib <ahrajib6789@gmail.com>
2025-01-22 03:16:41 +0600

    Add a nested GET endpoint (to get all reviews on a specific tour)

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,7 +5,11 @@ import catchAsync from '../utils/catchAsync.js';
 // ============
 
 const getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
