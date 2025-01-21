401036689d05714002403847f2918b48d28c3cc2
Arman Rajib <ahrajib6789@gmail.com>
2025-01-21 18:51:51 +0600

    Implement creating and getting reviews

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import hpp from 'hpp';
 
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import reviewRouter from './routes/reviewRoutes.js';
 import AppError from './utils/appError.js';
 import globalErrorHandler from './controllers/errorController.js';
 
@@ -94,6 +95,7 @@ app.use(express.static(`${appRoot}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // UNHANDLED ROUTES MIDDLEWARE
 // ============================
---
+++ controllers/reviewController.js
@@ -0,0 +1,30 @@
+import Review from '../models/reviewModel.js';
+import catchAsync from '../utils/catchAsync.js';
+
+// CONTROLLERS
+// ============
+
+const getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+const createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
+
+export { getAllReviews, createReview };
