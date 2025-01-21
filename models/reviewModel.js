fca637b7da710baef4bfde773c74537cd0a0afb4
Arman Rajib <ahrajib6789@gmail.com>
2025-01-21 19:04:58 +0600

    Implement populating [user] and [tour] fields in [review] documents

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,22 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+// QUERY MIDDLEWARE
+// =================
+
+// Populate the user and tour fields in the review
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 export default Review;
