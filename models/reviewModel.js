08a6bd115bad76c0f0c8c647811c3a0f0913ece5
Arman Rajib <ahrajib6789@gmail.com>
2025-01-23 01:47:14 +0600

    Prevent duplicate reviews for a particular user, and round the ratingsAverage value

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,12 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+// INDEXES
+// ========
+
+// Preventing duplicate reviews
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 // QUERY MIDDLEWARE
 // =================
 
