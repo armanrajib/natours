351cfab3bc7492f694097aa953457b1be0c692ff
Arman Rajib <ahrajib6789@gmail.com>
2025-01-23 20:57:46 +0600

    Build the tour overview page (part 1 & 2)

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,19 @@
-const getOverview = (req, res) => {
+import Tour from '../models/tourModel.js';
+import catchAsync from '../utils/catchAsync.js';
+
+const getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+  // This step has been implemented in the [views] folder in the [overview.pug] file
+
+  // 3) Render that template using tour data from (step 1)
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 const getTour = (req, res) => {
   res.status(200).render('tour', {
